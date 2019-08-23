a2820009bef11e5412311818ff11c2921398a5f7
Ramesh Singh <rameshsingh.jr@gmail.com>
2019-08-23 17:11:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -17,7 +17,7 @@ def reduce(array, sv = nil)
     sum = array[0]
     i = 1
   end
-  while i < array.length
-  sum = yield(sum, array[i])
-  i += 1
-  
+  while i < array.length do
+    sum = yield(sum, array[i])
+    i += 1
+    
